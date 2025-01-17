473351a59497f689161b3d55e6f8066bbf5b4a6a
rpsrivastava24337 <rpsrivastava24337@gmail.com>
2025-01-17 23:44:50 +0530

    feat-Add a Carousel in HomePage

---
+++ src/coustomer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomeSectionCard = () => {
+    return (
+        <div className='bg-white shadow-lg rounded-lg w-[15rem] cursor-pointer flex flex-col justify-center items-center overflow- hidden m-3'> 
+            <div className='w-[13] h-[10rem]'>
+            <img src='' alt='img' className='w-full h-full object-cover object-top' />
+
+            </div>
+        </div>
+    );
+};
+
+export default HomeSectionCard;
